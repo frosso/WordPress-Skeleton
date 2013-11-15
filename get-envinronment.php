8160538dc41add221ad59b7fa8193e698ad031e7
Francesco R <frosso89@gmail.com>
2013-11-15 20:27:44 +0100

    Funzione statica per envinronment

--- get-envinronment.php
+++ get-envinronment.php
@@ -7,12 +7,20 @@ class Envinronments {
     const local = 'lo';
     const staging = 'st';
     const production = 'pr';
+
+    public static function get_current_env( ) {
+        if ( !defined( 'DEV_ENV' ) ) {
+            if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+                define( 'DEV_ENV', Envinronments::local );
+            } elseif ( strpos( $_SERVER['SERVER_NAME'], 'remedia.me' ) !== false ) {
+                define( 'DEV_ENV', Envinronments::staging );
+            } else {
+                define( 'DEV_ENV', Envinronments::production );
+            }
+        }
+        return DEV_ENV;
+    }
+
 }
 
-if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
-    define( 'DEV_ENV', Envinronments::local );
-} elseif ( strpos( $_SERVER['SERVER_NAME'], 'remedia.me' ) !== false ) {
-    define( 'DEV_ENV', Envinronments::staging );
-} else {
-    define( 'DEV_ENV', Envinronments::production );
-}
+Envinronments::get_current_env( );
